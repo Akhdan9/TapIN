cd996c61f70f665a4ece75cf1cd2fc4998f6d635
Akhdan9 <akhdan.malang@gmail.com>
2022-03-11 09:07:06 +0700

    First Commit

---
+++ app/src/main/java/id/my/slametbsan/absensifirebase/ProfileActivity.java
@@ -0,0 +1,35 @@
+package id.my.slametbsan.absensifirebase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    TextInputLayout fullname, email, phone, angkatan;
+    TextView fullNameLabel, usernameLabel;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+//
+//    private void showAllUserData(){
+//        Intent intent = getIntent();
+//        String user_name = intent.getStringExtra("name");
+//        String user_email = intent.getStringExtra("email");
+//        String user_phone = intent.getStringExtra("phone");
+//        String user_angkatan = intent.getStringExtra("angkatan");
+//
+//        fullNameLabel.setText(user_name);
+//        fullname.getEditText.setText(user_name);
+//        fullNameLabel.setText(user_name);
+//        fullNameLabel.setText(user_name);
+//    }
+}
